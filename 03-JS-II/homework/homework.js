4b96b2ce3b9fe2ab758f798661cf7ce71bc67ce9
mmachu <matiasmachulini@gmail.com>
2021-11-19 14:50:14 -0300

    completes homework JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -134,8 +134,10 @@ function fizzBuzz(numero) {
     return "fizzbuzz";
   } else if (numero % 3 === 0) {
     return "fizz";
-  } else {
+  } else if (numero % 5 === 0) {
     return "buzz";
+  } else {
+    return numero;
   }
 }
 
@@ -147,14 +149,14 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
 
-  if (num1 > num2 && num1 > num2 && num1 > 0) {
-    return "Numero 1 es mayor y positivo";
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
   } else if (num1 < 0 || num2 < 0 || num3 < 0) {
     return "Hay negativos";
   } else if (num3 > num1 && num3 > num2) {
     return num3 + 1;
-  } else if (num1 === 0 && num2 === 0 && num3 === 0) {
-    return "Error";
+  } else if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return "Número 1 es mayor y positivo";
   } else {
     return false;
   }
